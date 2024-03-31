2dd3906e70feea27cd4a41e7df64e54c45a87ea1
n-gal <147702076+n-gal@users.noreply.github.com>
2024-03-31 15:11:27 +0200

    Added DefinedColorFragmentShader shader

--- Engine.cpp
+++ Engine.cpp
@@ -10,25 +10,6 @@ void processInput(GLFWwindow* window);
 void drawTitle();
 void setupCMDWindowParams();
 
-const char* definedColourVertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColour;\n"
-"out vec3 vertColour;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"   vertColour = aColour;\n"
-"}\0";
-
-const char* definedColourfragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 vertColour;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(vertColour, 1.0f);\n"
-"}\n\0";
-
-
 int main()
 {
     setupCMDWindowParams();
@@ -63,7 +44,7 @@ int main()
     //-------------------------------------------------------------
     // Set up vertex data
     //-------------------------------------------------------------
-    float vertices[] = {
+    float rectangle[] = {
          0.5f,  0.5f, 0.0f,  // top right
          0.5f, -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  // bottom left
@@ -74,14 +55,31 @@ int main()
         1, 2, 3    // second triangle
     };
     
-    /*
-    float vertices[] = {
+
+    float triangleVertices[] = {
+        // positions        
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    
+    float colorVertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  0.5f, 0.5f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
          0.0f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f    // top 
     };
-    */
+    
+
+
+    //-------------------------------------------------------------
+    // Set up texture coordinate data
+    //-------------------------------------------------------------
+    float texCoords[] = {
+    0.0f, 0.0f,  // lower-left corner  
+    1.0f, 0.0f,  // lower-right corner
+    0.5f, 1.0f   // top-center corner
+    };
 
 
 
@@ -95,23 +93,24 @@ int main()
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
     
-    
+    /*
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
     
 
 
     //-------------------------------------------------------------
     // Set up how vertex data is read
     //-------------------------------------------------------------
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    
+
     /*
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -129,6 +128,7 @@ int main()
     Shader VertexColorShader("VertexColorVertexShader.glsl", "VertexColorFragmentShader.glsl");
     Shader BaseShader("BaseVertexShader.glsl", "BaseFragmentShader.glsl");
     Shader MovingShader("MovingVertexShader.glsl", "MovingFragmentShader.glsl");
+    Shader DefinedColorShader("DefinedColorVertexShader.glsl", "DefinedColorFragmentShader.glsl");
 
 
 
@@ -142,8 +142,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        MovingShader.use();
-        
+        BaseShader.use();
+  
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         float vertValue = (sin(timeValue * 20));
@@ -152,8 +152,8 @@ int main()
         MovingShader.set4Float("NewColor", 0.0f, greenValue, 0.0f, 1.0f);
 
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
