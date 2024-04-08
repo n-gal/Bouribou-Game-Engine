01b8e6d9bc8827c97e25ca00adf7d62e18c10c09
n-gal <147702076+n-gal@users.noreply.github.com>
2024-04-08 16:40:54 +0200

    spinning camera

--- Engine.cpp
+++ Engine.cpp
@@ -254,8 +254,20 @@ int main()
         //glm::mat4 model = glm::mat4(1.0f);
         //model = glm::rotate(model, glm::radians(-50.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        //glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);  
+        //glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        //glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+        //glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        //glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+        //glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+
+
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        glm::mat4 view;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
