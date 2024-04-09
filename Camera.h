1faa888935961b8d9d78608aad02792192829e1a
n-gal <147702076+n-gal@users.noreply.github.com>
2024-04-09 13:56:21 +0200

    Added awesome comments :+1:

--- Camera.h
+++ Camera.h
@@ -5,7 +5,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+
+
+//-------------------------------------------------------------
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+//-------------------------------------------------------------
 enum Camera_Movement {
     FORWARD,
     BACKWARD,
@@ -13,7 +17,11 @@ enum Camera_Movement {
     RIGHT
 };
 
+
+
+//-------------------------------------------------------------
 // Default camera values
+//-------------------------------------------------------------
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float SPEED = 2.5f;
@@ -21,7 +29,10 @@ const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
 
+
+//-------------------------------------------------------------
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+//-------------------------------------------------------------
 class Camera
 {
 public:
@@ -39,30 +50,63 @@ public:
     float MouseSensitivity;
     float Zoom;
 
+
+
+    //-------------------------------------------------------------
     // constructor with vectors
+    //-------------------------------------------------------------
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
 
+
+
+    //-------------------------------------------------------------
     // constructor with scalar values
+    //-------------------------------------------------------------
     Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
 
+
+
+    //-------------------------------------------------------------
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    //-------------------------------------------------------------
     glm::mat4 GetViewMatrix();
 
+
+
+    //-------------------------------------------------------------
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    //-------------------------------------------------------------
     void ProcessKeyboard(Camera_Movement direction, float deltaTime);
 
+
+
+    //-------------------------------------------------------------
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    //-------------------------------------------------------------
     void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
 
+
+
+    //-------------------------------------------------------------
     // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    //-------------------------------------------------------------
     void ProcessMouseScroll(float yoffset);
-    //----------
+
+
+
+    //-------------------------------------------------------------
     // Resets the speed
+    //-------------------------------------------------------------
     void ResetSpeed();
  
 
 private:
+
+
+
+    //-------------------------------------------------------------
     // calculates the front vector from the Camera's (updated) Euler Angles
+    //-------------------------------------------------------------
     void updateCameraVectors();
 };
 #endif
