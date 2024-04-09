607eaa8df63a3243f0b0d790c55817233837de2b
n-gal <147702076+n-gal@users.noreply.github.com>
2024-04-09 02:23:20 +0200

    Made it so escape freed your mouse and clicking captured it

--- Engine.cpp
+++ Engine.cpp
@@ -367,7 +367,10 @@ void setupCMDWindowParams()
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 
     float cameraSpeed = 2.5f * deltaTime;
 
