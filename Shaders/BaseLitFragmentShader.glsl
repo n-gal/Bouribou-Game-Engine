3d2f0254e28dfd92f230f08d4e6cf066732501e9
n-gal <147702076+n-gal@users.noreply.github.com>
2024-04-11 12:13:29 +0200

    Added proto material struct

--- Shaders/BaseLitFragmentShader.glsl
+++ Shaders/BaseLitFragmentShader.glsl
@@ -1,6 +1,13 @@
 #version 330 core
 out vec4 FragColor;
 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
